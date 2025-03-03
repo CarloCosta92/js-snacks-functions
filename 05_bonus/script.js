c3cf228e0c137565275a6fd534fb1e4100ddf8b0
Carlo Costa <carlo.costa92@gmail.com>
2025-03-03 16:38:24 +0100

    feat: solved bonus

---
+++ 05_bonus/script.js
@@ -0,0 +1,50 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+Il saluto deve essere :
+buongiorno se è mattina (fino alle 13), 
+buon pomeriggio se è pomeriggio (fino alle 17) 
+e buonasera se è sera (oltre le 17)
+*/
+
+const nome = 'Carlo';
+
+
+// Dichiara la funzione qui.
+function salutare(nome) {
+    const hours = new Date().getHours();
+    let saluto;
+    if (hours <= 13){
+        saluto="Buongiorno"
+    } else if(hours <= 17){
+        saluto="Buon Pomeriggio"
+    }else{
+        saluto="Buonasera"
+    }
+
+    return `${saluto} ${nome}`;
+}
+
+//arrow functions
+
+// const salutare = (nome) => {
+//     const hours = new Date().getHours();
+//     let saluto;
+//     if (hours <= 13){
+//         saluto="Buongiorno"
+//     } else if(hours <= 17){
+//         saluto="Buon Pomeriggio"
+//     }else{
+//         saluto="Buonasera"
+//     }
+
+//     return `${saluto} ${nome}`;
+// }
+
+
+
+
+// Invoca la funzione qui e stampa il risultato in console
+
+salutare(nome);
+console.log(salutare(nome));
+
+//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
